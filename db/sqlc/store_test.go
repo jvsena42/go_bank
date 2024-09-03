3385d66ff05d9fb3cf0f97bd723a6d323b518cf7
Joao Victor Sena <jvsena16@gmail.com>
2024-09-03 14:47:42 -0300

    test: TestTransfersx

---
+++ db/sqlc/store_test.go
@@ -0,0 +1,78 @@
+package db
+
+import (
+	"context"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestTransfersx(t *testing.T) {
+	store := NewStore(testDB)
+
+	account1 := createRandomAccount()
+	account2 := createRandomAccount()
+
+	// run n concurrent transfer transactions
+	n := 5
+	amount := int64(10)
+
+	errors := make(chan error)
+	results := make(chan TransferTxResult)
+
+	for range n {
+		go func() {
+			result, err := store.TransferTx(context.Background(), TransferTxParams{
+				FromAccountID: account1.ID,
+				ToAccountID:   account2.ID,
+				Amount:        amount,
+			})
+
+			errors <- err
+			results <- result
+		}()
+	}
+
+	for range n {
+		err := <-errors
+		require.NoError(t, err)
+
+		result := <-results
+		require.NotEmpty(t, result)
+
+		//check transfer
+		transfer := result.Transfer
+		require.NotEmpty(t, transfer)
+		require.Equal(t, account1.ID, transfer.FromAccountID)
+		require.Equal(t, account2.ID, transfer.ToAccountID)
+		require.Equal(t, amount, transfer.Amount)
+		require.NotZero(t, transfer.CreatedAt)
+		require.NotZero(t, transfer.ID)
+
+		_, err = store.GetTransfer(context.Background(), transfer.ID)
+		require.NoError(t, err)
+
+		//Check entries
+		fromEntry := result.FromEntry
+		require.NotEmpty(t, fromEntry)
+		require.Equal(t, account1.ID, fromEntry.AccountID)
+		require.Equal(t, -amount, fromEntry.Amount)
+		require.NotZero(t, fromEntry.ID)
+		require.NotZero(t, fromEntry.CreatedAt)
+
+		_, err = store.GetEntry(context.Background(), fromEntry.ID)
+		require.NoError(t, err)
+
+		toEntry := result.ToEntry
+		require.NotEmpty(t, toEntry)
+		require.Equal(t, account2.ID, toEntry.AccountID)
+		require.Equal(t, amount, toEntry.Amount)
+		require.NotZero(t, toEntry.ID)
+		require.NotZero(t, toEntry.CreatedAt)
+
+		_, err = store.GetEntry(context.Background(), toEntry.ID)
+		require.NoError(t, err)
+
+		//TODO Check account's balances
+	}
+}
