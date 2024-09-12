c7eb0a5f22ea06b36977289ab2556abf7d7a054f
Joao Victor Sena <jvsena16@gmail.com>
2024-09-12 14:45:32 -0300

    feat: update account balance

--- db/query/accounts.sql
+++ db/query/accounts.sql
@@ -29,6 +29,12 @@ SET balance = $2
 WHERE id = $1
 RETURNING *;
 
+-- name: AddAccountBalance :one
+UPDATE account
+SET balance = balance + sqlc.arg(amount)
+WHERE id = sqlc.arg(id)
+RETURNING *;
+
 -- name: DeleteAccount :exec
 DELETE FROM account
 WHERE id = $1;
