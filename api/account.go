bf61853bd03f017d9de43307c2dd31630d4316fd
Joao Victor Sena <jvsena16@gmail.com>
2024-09-16 14:15:27 -0300

    feat: list accounts

--- api/account.go
+++ api/account.go
@@ -63,29 +63,28 @@ func (server *Server) getAccount(ctx *gin.Context) {
 	ctx.JSON(http.StatusCreated, account)
 }
 
-/*
 type listAccountRequest struct {
 	PageID   int32 `form:"page_id" binding:"required,min=1"`
-	PageSize int32 `form:"page_size" binding:"required,min=1"`
+	PageSize int32 `form:"page_size" binding:"required,min=5,max=20"`
 }
 
 func (server *Server) listAccount(ctx *gin.Context) {
 	var req listAccountRequest
-	if err := ctx.ShouldBindUri(&req); err != nil {
+	if err := ctx.ShouldBindQuery(&req); err != nil {
 		ctx.JSON(http.StatusBadRequest, errorResponse(err))
 		return
 	}
 
-	accounts, err := server.store.ListAccounts(ctx, req.ID)
-	if err != nil {
-		if err == sql.ErrNoRows {
-			ctx.JSON(http.StatusNotFound, errorResponse(err))
-			return
-		}
+	arg := db.ListAccountsParams{
+		Limit:  req.PageSize,
+		Offset: (req.PageID - 1) * req.PageSize,
+	}
 
+	accounts, err := server.store.ListAccounts(ctx, arg)
+	if err != nil {
 		ctx.JSON(http.StatusInternalServerError, errorResponse(err))
 		return
 	}
 
-	ctx.JSON(http.StatusCreated, account)
-}*/
+	ctx.JSON(http.StatusCreated, accounts)
+}
