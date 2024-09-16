e8d94f1b5e0f1315ad29a2dd5fefb5ce61c24a01
Joao Victor Sena <jvsena16@gmail.com>
2024-09-16 13:50:50 -0300

    fix: get account url bind

--- api/account.go
+++ api/account.go
@@ -1,6 +1,8 @@
 package api
 
 import (
+	"database/sql"
+	"fmt"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
@@ -8,8 +10,8 @@ import (
 )
 
 type CreateAccountRequest struct {
-	Owner    string `json:"owner" bindin:"required"`
-	Currency string `json:"currency" bindin:"required, oneof= USD EUR BTC"`
+	Owner    string `json:"owner" binding:"required"`
+	Currency string `json:"currency" binding:"required,oneof= USD EUR BTC"`
 }
 
 func (server *Server) createAccount(ctx *gin.Context) {
@@ -33,3 +35,57 @@ func (server *Server) createAccount(ctx *gin.Context) {
 
 	ctx.JSON(http.StatusCreated, account)
 }
+
+type getAccountRequest struct {
+	ID int64 `uri:"id" binding:"required,min=1"`
+}
+
+func (server *Server) getAccount(ctx *gin.Context) {
+	var req getAccountRequest
+	if err := ctx.ShouldBindUri(&req); err != nil {
+		fmt.Println("Error getAccount, request:", req)
+		ctx.JSON(http.StatusBadRequest, errorResponse(err))
+		return
+	}
+
+	account, err := server.store.GetAccount(ctx, req.ID)
+	if err != nil {
+		fmt.Println("Error getAccount, request:", req)
+		if err == sql.ErrNoRows {
+			ctx.JSON(http.StatusNotFound, errorResponse(err))
+			return
+		}
+
+		ctx.JSON(http.StatusInternalServerError, errorResponse(err))
+		return
+	}
+
+	ctx.JSON(http.StatusCreated, account)
+}
+
+/*
+type listAccountRequest struct {
+	PageID   int32 `form:"page_id" binding:"required,min=1"`
+	PageSize int32 `form:"page_size" binding:"required,min=1"`
+}
+
+func (server *Server) listAccount(ctx *gin.Context) {
+	var req listAccountRequest
+	if err := ctx.ShouldBindUri(&req); err != nil {
+		ctx.JSON(http.StatusBadRequest, errorResponse(err))
+		return
+	}
+
+	accounts, err := server.store.ListAccounts(ctx, req.ID)
+	if err != nil {
+		if err == sql.ErrNoRows {
+			ctx.JSON(http.StatusNotFound, errorResponse(err))
+			return
+		}
+
+		ctx.JSON(http.StatusInternalServerError, errorResponse(err))
+		return
+	}
+
+	ctx.JSON(http.StatusCreated, account)
+}*/
