8cb34cfa0a3f742511589b9e5c9cf847fd7992b7
Joao Victor Sena <jvsena16@gmail.com>
2024-09-19 14:02:31 -0300

    refactor: add config file

--- main.go
+++ main.go
@@ -6,17 +6,19 @@ import (
 
 	"github.com/jvsena42/go_bank/api"
 	db "github.com/jvsena42/go_bank/db/sqlc"
+	"github.com/jvsena42/go_bank/util"
 	_ "github.com/lib/pq"
 )
 
-const (
-	dbDriver      = "postgres"
-	dbSource      = "postgresql://root:secret@localhost:5432/go_bank?sslmode=disable"
-	serverAddress = "0.0.0.0:8080"
-) //TODO USE ENVIROMENT VARIABLES
-
 func main() {
-	conn, err := sql.Open(dbDriver, dbSource)
+	config, err := util.LoadConfig(".")
+
+	if err != nil {
+		log.Fatal("error loading config: ", err)
+		return
+	}
+
+	conn, err := sql.Open(config.DBDriver, config.DBSource)
 	if err != nil {
 		log.Fatal("cannot connect to db: ", err)
 	}
@@ -24,7 +26,7 @@ func main() {
 	store := db.NewStore(conn)
 	server := api.NewServer(store)
 
-	err = server.Start(serverAddress)
+	err = server.Start(config.ServerAddress)
 
 	if err != nil {
 		log.Fatal("cannot start server: ", err)
