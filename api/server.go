483d8883d548df14b83f198f91bc5ebc996ce043
Joao Victor Sena <jvsena16@gmail.com>
2024-09-21 15:24:30 -0300

    feat: add endpoint

--- api/server.go
+++ api/server.go
@@ -18,6 +18,8 @@ func NewServer(store db.Store) *Server {
 	router.GET("/accounts/:id", server.getAccount)
 	router.GET("/accounts", server.listAccount)
 
+	router.POST("/transfers", server.CreateTransfer)
+
 	server.router = router
 	return server
 
