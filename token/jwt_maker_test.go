9e3857c4e8b994070a433daabb444c54d436de86
Joao Victor Sena <jvsena16@gmail.com>
2024-11-15 19:15:05 -0300

    test: jwt test

---
+++ token/jwt_maker_test.go
@@ -0,0 +1,33 @@
+package token
+
+import (
+	"testing"
+	"time"
+
+	"github.com/jvsena42/go_bank/util"
+	"github.com/stretchr/testify/require"
+)
+
+func TestJWTMaker(t *testing.T) {
+	maker, err := NewJWTMaker(util.RandomString(32))
+	require.NoError(t, err)
+
+	userName := util.RandomName()
+	duration := time.Minute
+
+	issuedAt := time.Now()
+	expireAt := issuedAt.Add(duration)
+
+	token, err := maker.CreateToken(userName, duration)
+	require.NoError(t, err)
+	require.NotEmpty(t, token)
+
+	payload, err := maker.VerifyToken(token)
+	require.NoError(t, err)
+	require.NotEmpty(t, payload)
+
+	require.NotZero(t, payload.ID)
+	require.Equal(t, userName, payload.Username)
+	require.WithinDuration(t, issuedAt, payload.IssuedAt, time.Second)
+	require.WithinDuration(t, expireAt, payload.ExpireAt, time.Second)
+}
