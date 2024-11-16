f203f7e3683b3d2ece4b3de56afe2fa5f9646964
Joao Victor Sena <jvsena16@gmail.com>
2024-11-16 08:34:13 -0300

    feat: token enviroment variables

--- api/server.go
+++ api/server.go
@@ -1,19 +1,34 @@
 package api
 
 import (
+	"fmt"
+
 	"github.com/gin-gonic/gin"
 	"github.com/gin-gonic/gin/binding"
 	"github.com/go-playground/validator/v10"
 	db "github.com/jvsena42/go_bank/db/sqlc"
+	"github.com/jvsena42/go_bank/token"
+	"github.com/jvsena42/go_bank/util"
 )
 
 type Server struct {
+	config util.Config
 	store  db.Store
+	maker  token.Maker
 	router *gin.Engine
 }
 
-func NewServer(store db.Store) *Server {
-	server := &Server{store: store}
+func NewServer(config util.Config, store db.Store) (*Server, error) {
+	tokenMaker, err := token.NewPasetoMaker(config.TokenSymmetricKey)
+	if err != nil {
+		return nil, fmt.Errorf("cannot create token maker: %w", err)
+	}
+
+	server := &Server{
+		config: config,
+		store:  store,
+		maker:  tokenMaker,
+	}
 	router := gin.Default()
 
 	if v, ok := binding.Validator.Engine().(*validator.Validate); ok {
@@ -29,7 +44,7 @@ func NewServer(store db.Store) *Server {
 	router.POST("/transfers", server.CreateTransfer)
 
 	server.router = router
-	return server
+	return server, nil
 
 }
 
