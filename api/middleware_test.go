8b6ead878713a379fdc2d06558a15efbff069474
Joao Victor Sena <jvsena16@gmail.com>
2024-11-18 08:10:18 -0300

    test: addAuthorization test

--- api/middleware_test.go
+++ api/middleware_test.go
@@ -1,21 +1,48 @@
 package api
 
 import (
+	"fmt"
 	"net/http"
 	"net/http/httptest"
 	"testing"
+	"time"
 
 	"github.com/gin-gonic/gin"
 	"github.com/jvsena42/go_bank/token"
 	"github.com/stretchr/testify/require"
 )
 
+func addAuthorization(
+	t *testing.T,
+	request *http.Request,
+	tokenMaker token.Maker,
+	authorizationType string,
+	userName string,
+	duration time.Duration,
+) {
+	token, err := tokenMaker.CreateToken(userName, duration)
+	require.NoError(t, err)
+
+	authorizationHeader := fmt.Sprintf("%s %s", authorizationType, token)
+	request.Header.Set(authorizationHeaderKey, authorizationHeader)
+}
+
 func TestAuthMiddleware(t *testing.T) {
 	testCases := []struct {
 		name          string
 		setupAuth     func(t *testing.T, request *http.Request, tokenMaker token.Maker)
 		checkResponse func(t *testing.T, recorder *httptest.ResponseRecorder)
-	}{}
+	}{
+		{
+			name: "OK",
+			setupAuth: func(t *testing.T, request *http.Request, tokenMaker token.Maker) {
+				addAuthorization(t, request, tokenMaker, authorizationTypeBearer, "userTest", time.Minute)
+			},
+			checkResponse: func(t *testing.T, recorder *httptest.ResponseRecorder) {
+				require.Equal(t, http.StatusOK, recorder.Code)
+			},
+		},
+	}
 
 	for i := range testCases {
 		tc := testCases[i]
@@ -39,6 +66,7 @@ func TestAuthMiddleware(t *testing.T) {
 
 				tc.setupAuth(t, request, server.maker)
 				server.router.ServeHTTP(recorder, request)
+				tc.checkResponse(t, recorder)
 			})
 	}
 }
